6cc36683d31535dbffeab1ea10ea1e741321a88e
star-think <qaws2921@hanmail.net>
2022-06-30 23:04:09 +0900

    일기 수정, Lifecycle 제어

--- src/App.js
+++ src/App.js
@@ -2,31 +2,7 @@ import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-/*
-const dummyList = [
-  {
-    id: 1,
-    author: "김종효",
-    content: "하이 1",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "최은정",
-    content: "하이 2",
-    emotion: 2,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "이선",
-    content: "하이 3",
-    emotion: 1,
-    created_date: new Date().getTime(),
-  },
-];
-*/
+import Lifecycle from "./Lifecycle";
 
 function App() {
   const [data, setData] = useState([]);
@@ -47,15 +23,24 @@ function App() {
     setData([newItem, ...data]);
   };
 
-  const onDelete = (targetId) => {
+  const onRemove = (targetId) => {
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
   };
 
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+  };
+
   return (
     <div className="App">
+      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList onDelete={onDelete} dummyList={data} />
+      <DiaryList onEdit={onEdit} onRemove={onRemove} dummyList={data} />
     </div>
   );
 }
--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,14 +1,14 @@
 import React from "react";
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({ onDelete, dummyList }) => {
+const DiaryList = ({ onEdit, onRemove, dummyList }) => {
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
       <h4>{dummyList.length}개의 일기가 있습니다.</h4>
       <div>
         {dummyList.map((it, idx) => (
-          <DiaryItem key={it.id} {...it} onDelete={onDelete} />
+          <DiaryItem key={it.id} {...it} onRemove={onRemove} onEdit={onEdit} />
         ))}
       </div>
     </div>
